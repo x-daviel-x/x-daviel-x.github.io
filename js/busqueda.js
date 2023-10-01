68edd8240eedd32468ddd36b554896004e35a77e
x-daviel-x <luisdaviel13@gmail.com>
2023-09-30 20:25:41 -0400

    Nuevo diseño de Barra de Búsqueda y se añadió la Política de Privacidad
    En esta actualización se ha rediseñado el listado de los resultados en la Barra Búsqueda, siendo que ahora se muestran imágenes o iconos en función de cada resultado. También se ha incorporado una versión preliminar a la Política de Privacidad.

--- js/busqueda.js
+++ js/busqueda.js
@@ -1,8 +1,8 @@
 const elements = [
-    { title: "Berserks", subtitle: "Franquicia", link: "../inicio/fichas/franquicias/berserk/fr.html" },
-    { title: "Afro Samurai", subtitle: "(Anime)", link: "../inicio/fichas/animes/afro samurai/ft.html" },
-    { title: "Akira", subtitle: "(Pelicula)", link: "../inicio/fichas/peliculas/akira/ft.html" },
-    { title: "La chica que saltaba a través del tiempo", subtitle: "Franquicia",                 link: "../inicio/fichas/franquicias/berserk/fr.html" },
+    { title: "Afro Samurai", subtitle: "Anime", link: "../inicio/fichas/animes/afro samurai/ft.html" },
+    { title: "Demon Slayer", subtitle: "Franquicia", link: "../inicio/fichas/franquicias/demon_slayer/fr.html" },
+    { title: "Demon Slayer A", subtitle: "Anime", link: "../inicio/fichas/franquicias/demon_slayer/animes/demon slayer/ft.html" },
+    { title: "Akira", subtitle: "Pelicula", link: "../inicio/fichas/peliculas/akira/ft.html" },
 ];
 
 const searchInput = document.getElementById("searchInput");
@@ -31,12 +31,33 @@ searchInput.addEventListener("input", function () {
 
             const link = document.createElement("a");
             link.href = element.link;
-            link.innerHTML = `
+
+            // Crea una imagen
+            const image = document.createElement("img");
+            // Establece la ruta de la imagen basada en el título
+            image.src = `${element.title}.png`;
+            // Establece un atributo alt para la accesibilidad
+            image.alt = element.title;
+
+            // Agrega la imagen al resultado
+            listItem.appendChild(image);
+
+            // Crea un contenedor para el texto del resultado
+            const resultText = document.createElement("div");
+            resultText.classList.add("result-text");
+
+            // Agrega el título y el subtítulo al contenedor de texto
+            resultText.innerHTML = `
                 <div class="title">${element.title}</div>
                 <div class="subtitle">${element.subtitle}</div>
             `;
 
+            // Agrega el contenedor de texto al enlace
+            link.appendChild(resultText);
+
+            // Agrega el enlace a la lista de resultados
             listItem.appendChild(link);
+
             resultsList.appendChild(listItem);
         });
     }
