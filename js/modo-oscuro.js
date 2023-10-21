fd08948e8f1cefb7a073c0a5cbefa7593c7f301c
x-daviel-x <luisdaviel13@gmail.com>
2023-10-20 20:11:10 -0400

    Modo Oscuro en Línea
    Se han realizado unos pequeños cambios el código .js del modo oscuro para que pueda funcionar en la web.

--- js/modo-oscuro.js
+++ js/modo-oscuro.js
@@ -1,29 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
     const toggleButton = document.getElementById('cambiar-modo');
 
-    // Verifica el modo actual almacenado en el localStorage
-    const isModoOscuro = localStorage.getItem('modoOscuro') === 'activado';
+    // Verifica el modo actual almacenado en las cookies
+    const cookies = document.cookie;
+    const isModoOscuro = cookies.includes('modoOscuro=activado');
 
     // Aplica el modo actual
     setMode(isModoOscuro);
 
     // Escucha los clics en el botón para alternar modos
     toggleButton.addEventListener('click', () => {
-        const currentMode = localStorage.getItem('modoOscuro');
-        if (currentMode === 'activado') {
-            setMode(false);
-        } else {
-            setMode(true);
-        }
+        const currentMode = document.cookie.includes('modoOscuro=activado');
+        setMode(!currentMode);
     });
-    
+
     function setMode(estaOscuro) {
         if (estaOscuro) {
             document.body.classList.add('modo-oscuro');
-            localStorage.setItem('modoOscuro', 'activado');
+            document.cookie = 'modoOscuro=activado; expires=Fri, 31 Dec 9999 23:59:59 GMT';
         } else {
             document.body.classList.remove('modo-oscuro');
-            localStorage.setItem('modoOscuro', null);
+            document.cookie = 'modoOscuro=; expires=Thu, 01 Jan 1970 00:00:00 GMT';
         }
     }
 });
