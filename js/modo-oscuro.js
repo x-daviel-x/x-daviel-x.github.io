6defd9281e1cf194a7e81fde69eaead793b452c7
x-daviel-x <luisdaviel13@gmail.com>
2023-10-20 22:33:20 -0400

    Modo Oscuro en Línea v4
    Esta pequeña actualización pretende arreglar un fallo con la cookie del modo oscuro.

--- js/modo-oscuro.js
+++ js/modo-oscuro.js
@@ -1,32 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleButton = document.getElementById('cambiar-modo');
-
-    // Verifica el modo actual almacenado en el localStorage
-    const isModoOscuro = localStorage.getItem('modoOscuro') === 'activado';
-
-    // Aplica el modo actual
-    setMode(isModoOscuro);
-
-    // Escucha los clics en el botón para alternar modos
-    toggleButton.addEventListener('click', () => {
-        const currentMode = localStorage.getItem('modoOscuro');
-        if (currentMode === 'activado') {
-            setMode(false);
-        } else {
-            setMode(true);
-        }
-    });
-    
-    function setMode(estaOscuro) {
-        if (estaOscuro) {
-            document.body.classList.add('modo-oscuro');
-            localStorage.setItem('modoOscuro', 'activado');
-        } else {
-            document.body.classList.remove('modo-oscuro');
-            localStorage.setItem('modoOscuro', null);
-        }
-    }
-});
 
 document.addEventListener('DOMContentLoaded', () => {
     const toggleButton = document.getElementById('cambiar-modo');
@@ -48,6 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (estaOscuro) {
             document.body.classList.add('modo-oscuro');
             document.cookie = 'modoOscuro=activado; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/';
+
         } else {
             document.body.classList.remove('modo-oscuro');
             document.cookie = 'modoOscuro=; expires=Thu, 01 Jan 2970 00:00:00 GMT';
