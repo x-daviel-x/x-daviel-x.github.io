41020438fefd9b5e8e417bb9a72f359243444db7
x-daviel-x <luisdaviel13@gmail.com>
2023-10-20 22:54:24 -0400

    Modo Oscuro v6
    Esta pequeña actualización pretende arreglar un fallo con la cookie del modo oscuro.

--- js/modo-oscuro.js
+++ js/modo-oscuro.js
@@ -1,49 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
     const toggleButton = document.getElementById('cambiar-modo');
 
-    // Verifica el modo actual almacenado en una cookie
-    const isModoOscuro = getCookie('modoOscuro') === 'activado';
+    // Verifica el modo actual almacenado en las cookies
+    const cookies = document.cookie;
+    const isModoOscuro = cookies.includes('modoOscuro=activado');
 
     // Aplica el modo actual
     setMode(isModoOscuro);
 
     // Escucha los clics en el botón para alternar modos
     toggleButton.addEventListener('click', () => {
-        const currentMode = getCookie('modoOscuro');
-        if (currentMode === 'activado') {
-            setMode(false);
-        } else {
-            setMode(true);
-        }
+        const currentMode = document.cookie.includes('modoOscuro=activado');
+        setMode(!currentMode);
     });
 
     function setMode(estaOscuro) {
         if (estaOscuro) {
             document.body.classList.add('modo-oscuro');
-            setCookie('modoOscuro', 'activado', 365); // Establece una cookie con una duración de 365 días
+            document.cookie = 'modoOscuro=activado; expires=Fri, 31 Dec 9999 23:59:59 GMT';
         } else {
             document.body.classList.remove('modo-oscuro');
-            deleteCookie('modoOscuro');
+            document.cookie = 'modoOscuro=; expires=Thu, 01 Jan 1970 00:00:00 GMT';
         }
-    }
-
-    // Función para obtener el valor de una cookie
-    function getCookie(name) {
-        const value = `; ${document.cookie}`;
-        const parts = value.split(`; ${name}=`);
-        if (parts.length === 2) return parts.pop().split(';').shift();
-    }
-
-    // Función para establecer una cookie
-    function setCookie(name, value, days) {
-        const date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        const expires = `expires=${date.toUTCString()}`;
-        document.cookie = `${name}=${value}; ${expires}; path=/`;
-    }
-
-    // Función para eliminar una cookie
-    function deleteCookie(name) {
-        document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
     }
 });
