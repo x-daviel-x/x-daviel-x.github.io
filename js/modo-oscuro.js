51909691b2149ec81ec056b22a74030829ed1977
x-daviel-x <luisdaviel13@gmail.com>
2023-10-22 11:15:26 -0400

    Modo Oscuro v10
    Esta pequeña actualización pretende arreglar un pequeño fallo con la cookie del modo oscuro.

--- js/modo-oscuro.js
+++ js/modo-oscuro.js
@@ -19,10 +19,10 @@ document.addEventListener('DOMContentLoaded', () => {
     function setMode(estaOscuro) {
         if (estaOscuro) {
             document.body.classList.add('modo-oscuro');
-            document.cookie = 'modoOscuro=activado; expires=Fri, 31 Dec 9999 23:59:59 GMT';
+            document.cookie = 'modoOscuro=activado; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/'; // Path establecido en la raíz
         } else {
             document.body.classList.remove('modo-oscuro');
-            document.cookie = 'modoOscuro=; expires=Thu, 01 Jan 1970 00:00:00 GMT';
+            document.cookie = 'modoOscuro=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/'; // Path establecido en la raíz
         }
     }
 });
